ac772c1d5a7323d0da286d2f40012f4de456cfc9
Carlos <carlos@priv>
2019-03-17 08:37:36 +0100

    Initial upload of mkvm.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mkvm
-Files for quick creation of vm's with KVM and Kickstart for Lab purposes
+
+Files for quick creation of CentOS vm's with KVM and Kickstart for Lab purposes
+
+Requirements:
+
+- Folder with contents of CentOS7 ISO DVD
+- FTP Server with CentOS repo
+- Web server that serves ks.cfg
+- Optionally, Chef server to automatically add the vm's to Chef. 
+  You need to replace the validation key in the ks.cfg if you want to use this.
---
+++ mkvm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Effortlesly create CentOS vm's using kickstart and virt-install
+
+unset name
+unset octet
+
+domain="casacarlos.priv"
+host="192.168.122.1"
+template_kickstart="/var/www/html/install/ks.cfg"
+
+name=$1
+octet=$2
+
+if [[ -z "$name" ]]
+then
+	name="vm$RANDOM"
+fi
+
+if [[ -z "$octet" ]]
+then
+	octet="99"
+fi
+
+echo $name
+rm /var/www/html/install/ks_tmp.cfg
+cat /var/www/html/install/ks.cfg | sed s/HOSTNAME/$name.$domain.priv/g /var/www/html/install/ks.cfg | sed s/IPADDRESS/$octet/g > /var/www/html/install/ks_tmp.cfg
+virt-install \
+  --name $name\
+  --memory 4096 \
+  --vcpus 2 \
+  --disk size=20 \
+  --location http://192.168.122.1/install \
+  --os-variant rhel7 \
+  --extra-args "ks=http://192.168.122.1/install/ks_tmp.cfg" &
+
+echo "192.168.122.$octet $name.$domain.priv $name" >> /etc/hosts
